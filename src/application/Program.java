dbae2271fe46875adf5462cc739fc7b85c2dd530
Dayse Soares <daysedesign@gmail.com>
2019-11-11 14:35:25 -0300

    Atualizar codigo para trazer o novo id inserido

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -19,17 +21,26 @@ public class Program {
 			conn = DB.getConnection();
 			st = conn.prepareStatement("INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES (?, ?, ?, ?, ?)");
+					+ "VALUES (?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 			
-			st.setString(1, "Mariana");
-			st.setString(2, "mariana@gmail.com");
+			st.setString(1, "Rose");
+			st.setString(2, "rose@gmail.com");
 			st.setDate(3,  new java.sql.Date(sdf.parse("01/11/1980").getTime()));
 			st.setDouble(4, 1000.0);
 			st.setInt(5, 4);
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows Affeced: "+ rowsAffected);
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			} else {
+				System.out.println("No rown affected!");
+			}
 			
 		}catch (SQLException e) {
 			e.printStackTrace();
